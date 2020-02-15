450f8e2edf50a2bba13258550b462117900701ce
gabe <gabriel.simao@trojans.dsu.edu>
2020-02-15 01:07:21 -0600

    version 1.00
    first push

---
+++ Project 1/Assets/ImmbolileCollisionIgnoring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmbolileCollisionIgnoring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var go in GameObject.FindGameObjectsWithTag("Immobile"))
+        {
+            if (go.GetComponent<Collider>() != null && GetComponent<Collider>() != null)
+                Physics.IgnoreCollision(GetComponent<Collider>(), go.GetComponent<Collider>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
